bac36114eed1caa45dca2bc58a17b26193bccd05
HorizonCode <horizoncode88@gmail.com>
2024-08-05 10:50:42 +0200

    add supported categories

--- knaben.py
+++ knaben.py
@@ -1,4 +1,4 @@
-# VERSION: 1.0
+# VERSION: 1.2
 # AUTHORS: HorizonCode
 
 import json
@@ -11,9 +11,18 @@ from novaprinter import prettyPrinter
 class knaben(object):
     url = "https://knaben.eu"
     name = "Knaben Database"
-    supported_categories = {"all": ""}
+    supported_categories = {
+        "all": "",
+        "anime": [6000000, 6001000, 6002000, 6003000, 6004000, 6005000, 6006000, 6007000, 6008000],
+        "books": [9000000, 9001000, 9002000, 9003000, 9004000, 9005000],
+        "games": [4000000, 4001000],
+        "movies": [3000000, 3001000, 3002000, 3003000, 3004000, 3005000, 3006000, 3007000, 3008000],
+        "music": [1000000, 1001000, 1002000, 1003000, 1004000, 1005000, 1006000],
+        "software": [4002000, 4003000, 4004000],
+        "tv": [2000000, 2001000, 2002000, 2003000, 2004000, 2005000, 2006000, 2007000, 2008000]
+        }
 
-    def search(self, what, cat="all"):
+    def search(self, what, cat):
         what = unquote(what)
 
         search_url = "https://api.knaben.eu/v1"
@@ -27,6 +36,10 @@ class knaben(object):
             "hide_unsafe": True,
             "hide_xxx": False,
         }
+
+        if cat and cat != "all":
+            body["categories"] = self.supported_categories[cat]
+
         status, reason, response = self.request(search_url, body)
 
         if status < 200 or status > 299:
